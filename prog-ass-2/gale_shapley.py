6a8115bbfd9b34b4473ed88a4e0fc4c8d11e2c6d
shahbajsingh <ss4589@njit.edu>
2024-03-02 18:45:55 -0500

    Update gale_shapley.py
    stable

--- prog-ass-2/gale_shapley.py
+++ prog-ass-2/gale_shapley.py
@@ -2,96 +2,55 @@ import random
 import time
 import matplotlib.pyplot as plt
 
-def gen_prefs(n):
+def generate_preferences(n):
     return [random.sample(range(n), n) for _ in range(n)]
 
-def gale_shapley(n, m_prefs, w_prefs):
-    # inverse pref list for women
-    w_prefs_inv = [{} for _ in range(n)]
-    for w in range(n):
-        for rank, m in enumerate(w_prefs[w]):
-            w_prefs_inv[w][m] = rank
-
-    # free (unmatched) men list
-    free_m = list(range(n))
-
-    # proposals made by men to women
-    props = [0] * n
-
-    # women's matches (-1 means unmatched)
-    w_match_to = [-1] * n
-
-    while free_m:
-        man = free_m[0]
-        w_pref = m_prefs[man][props[man]]
-        props[man] += 1
-
-        if (w_match_to[w_pref] == -1):
-            w_match_to[w_pref] = man
-            free_m.pop(0)
-        else:
-            curr_man = w_match_to[w_pref]
-            if w_prefs_inv[w_pref][man] < w_prefs_inv[w_pref][curr_man]:
-                w_match_to[w_pref] = man
-                free_m[0] = curr_man
-
-    matches = [(m, w_match_to.index(m)) for m in range(n)]
-    # print formatted matches
-    print("Matches:")
-    for m, w in matches:
-        print(f"{m} -> {w}")
-    return matches
-
-def validate(n, m_prefs, w_prefs, matches):
-    woman_to_man = {w: m for m, w in matches}
-
-    for m, w in matches:
-        m_pref = m_prefs[m]
-        w_pref = w_prefs[w]
-
-        # check for any man this woman prefers more than her current partner
-        for w2 in m_pref[:m_pref.index(w)]:
-            m2 = woman_to_man[w2]
-            if w_prefs[w2].index(m) < w_prefs[w2].index(m2):
-                print(f'Unstable match: {w2} prefers {m} over {m2}')
-                return False
-            
-        # check for any woman this man prefers more than his current partner
-        for m2 in w_pref[:w_pref.index(m)]:
-            w2 = woman_to_man[m2]
-            if m_prefs[m2].index(w) < m_prefs[m2].index(w2):
-                print(f'Unstable match: {m2} prefers {w} over {w2}')
-                return False
-        print(f'Match: {m} and {w} are stable')
-    print("Matches are stable")
-    return True
-
-
-def main():
-    input_sizes = [10, 100, 1000, 10000, 100000]
-    times = []
-
-    for n in input_sizes:
-        m_prefs = gen_prefs(n)
-        w_prefs = gen_prefs(n)
-
-        start = time.time()
-        matches = gale_shapley(n, m_prefs, w_prefs)
-        end = time.time()
-        times.append(end - start)
-
-        if not validate(n, m_prefs, w_prefs, matches):
-            print("Validation failed")
-            return
+def gale_shapley(n, men_preferences, women_preferences):
+    women_inverse_pref = [{m: rank for rank, m in enumerate(women_preferences[w])} for w in range(n)]
+    free_men = set(range(n))
+    proposals = [0] * n
+    women_engaged_to = [-1] * n
+    
+    while free_men:
+        man = free_men.pop()
+        woman = men_preferences[man][proposals[man]]
+        proposals[man] += 1
         
-    plt.plot(input_sizes, times, 'o-')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.xlabel('Input Size (n)')
-    plt.ylabel('Time (s)')
-    plt.title('Gale-Shapley Algorithm')
-    plt.show()
+        if women_engaged_to[woman] == -1:
+            women_engaged_to[woman] = man
+        else:
+            current_partner = women_engaged_to[woman]
+            if women_inverse_pref[woman][man] < women_inverse_pref[woman][current_partner]:
+                women_engaged_to[woman] = man
+                free_men.add(current_partner)
+            else:
+                free_men.add(man)
+    
+    engagements = [(man, women_engaged_to.index(man)) for man in range(n)]
+    return engagements
 
+def validate_stability(n, engagements, men_preferences, women_preferences):
+    woman_to_man = {w: m for m, w in engagements}
+    man_to_woman = {m: w for m, w in engagements}
 
-if __name__ == "__main__":
-    main()
+    # Iterate through each woman to check her preferences
+    for woman, her_current_man in woman_to_man.items():
+        woman_pref = women_preferences[woman]
+        # Check if there's any man she prefers more than the current one
+        for preferred_man in woman_pref[:woman_pref.index(her_current_man)]:
+            # Get the woman whom the preferred man is currently engaged to
+            his_current_woman = man_to_woman[preferred_man]
+            # Check if the preferred man would also prefer this woman over his current partner
+            if men_preferences[preferred_man].index(woman) < men_preferences[preferred_man].index(his_current_woman):
+                return False, f"Instability found: Woman {woman} prefers Man {preferred_man} over her current partner Man {her_current_man}, and Man {preferred_man} also prefers Woman {woman} over his current partner Woman {his_current_woman}"
+
+    return True, "Stable matching confirmed"
+
+# Example with n = 4
+n = 100
+men_preferences = generate_preferences(n)
+women_preferences = generate_preferences(n)
+engagements = gale_shapley(n, men_preferences, women_preferences)
+stability, message = validate_stability(n, engagements, men_preferences, women_preferences)
+print(f"Engagements: {engagements}")
+print(f"Stability: {message}")

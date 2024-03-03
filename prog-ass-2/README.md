7c77f004e062e27a5c55ffa540c8be87d4288bfb
shahbajsingh <ss4589@njit.edu>
2024-03-02 23:58:45 -0500

    finalize

--- prog-ass-2/README.md
+++ prog-ass-2/README.md
@@ -8,12 +8,29 @@ Programming Assignment 2
 
 ##### Abstract
 
-Implementation of Gale-Shapely algorithm.
+Implementation of Gale-Shapely algorithm on lists of size `10`, `100`, `1000`, `10 000`, and `100 000` in order to visualize and validate the stability of the algorithm over various input sizes.
 
+I followed various implementations of the Gale-Shapley program from research I did online, but ultimately what was easiest for me was going back to my lecture notes from the first day, reworking the logic of the algorithm in my mind, and rewriting it thereafter in python. The bulk of the analysis draws from two function: `gale_shapely()`, the implementation of the algorithm itself, and `val_stable()`, which validates the stability of the match list returned by the former. Initially, I had trouble getting the algorithm to be proved stable, but I found myself erring actually in how I was comparing the matches– I found that I was mistakenly using the preference list of the original women in this loop:
+
+![loop](image.png)
+
+i.e., I had defined `w_pref` as `prefs_women[w]` in the outside loop; as you can see, this was corrected and `w_pref` was placed inside the correct loop where it could access the `w2` variable and correctly compare against `m_pref`.
 
 ____
 
  ##### Running the Program
+
+This program may be ran through the command line by `cd`ing into the working directory of the file [gale_shapley_.py](/prog-ass-2/gale_shapley_.py), and running the command
+
+`python3 gale_shapley_.py`
+
+Which will randomly generate preference lists of men and women of input sizes `n = 10, 100, 1000, 10000, 100000` and for each input size iteration, run the Gale-Shapley algorithm in order to create stable matches, and then run the validation function to prove that they are in fact stable matches. In my experience, validation takes significantly longer than actual execution of the algorithm (as `gale_shapley()` approaches $O(n^2)$, while `val_stable()` approaches $O(n^3)$ with a list lookup and comparison in its nested for-loop), so I also implemented a manual debugging feature which tracks the progress of matching, at the end of which validation begins.
+
+After validation has completed, the `.py` script will generate a plot of the execution times as a function of input size. It makes use of the following Python libraries:
+
+* `random`
+* `time`
+* `pyplot`– a module from `matplotlib`
 
  <br />
 
@@ -22,14 +39,53 @@ ____
 
 ##### Results
 
-<br />
 
+
+We find that, as expected, the execution time increases almost parabolically with input size as the time complexity of Gale-Shapley is $O(n^2)$. If we were to compare this to a brute-force implementation, say in the form of:
+
+<code>
+stable_match_forced(women, men, pref_rank_w, pref_rank_m)
+    
+    w_rank = Convert pref_rank_w to rank format
+    m_rank = Convert pref_rank_m to rank format
+
+    w_seq = Convert women set to sequence
+    Initialize matching collection with all possible pairings of women and men
+
+    For each possible match in matching collection
+        Create a dictionary match_w mapping each woman to her pair in the match
+        Create a dictionary match_m mapping each man to his pair in the match
+
+        Set unstable to False
+
+        For each woman w in women
+            For each man m in men
+                If w is not paired with m in match_m AND m is not paired with w in match_w
+                    If woman w prefers man m over her current partner AND man m prefers woman w over his current partner
+                        Set unstable to True
+                        Break inner loops
+
+        If the match is not unstable
+            Return the stable match
+</code>
+
+using the `permutations` module from the `itertools` library, we would probably not even be able to test it for comparison, as it would take on a time complexity of $O(n! \times n^2)$; Gale-Shapley is thus a far superior implementation of a stable matching algorithm, which is why it earned the Nobel Prize. The algorithm completes successfully in mere seconds; the overwhelming time and resource consumption of the program in [gale_shapley_.py](/prog-ass-2/gale_shapley_.py)is mainly the validation for actually checking that the stable match is valid (which it always is).
+
+Because the validation was so time consuming compared to running the algorithm, for `n = 100000`, I skipped the validation as it had already been proved stable over multiple runs, so that I could produce a plot within reasonable time. Displayed are the execution times I was able to extract, and the plot that was generated with them.
+
+![alt text](image-1.png)
+![alt text](image-2.png)
+![alt text](image-3.png)
 
 <br />
 
 ____
 
 ##### Performance Analysis
+
+The implementation of this algorithm as a logical continuation of the theory and pseudocode covered in class is proof that the Gale-Shapley algorithm always returns a stable matching. The validation function follows the logic of comparison discussed, by comparing, for each and every sample input, their preferences against that of the individual loop iterate in question. The successful production of the execution time plots against the varied-magnitude input sizes further adds to the validity of this claim.
+
+Beyond the time plots, analyzing the standard output logs proves that the Gale-Shapley algorithm produces a best-valid-match scenario for men and worst-valid-match scenario for women among stable matches as it is implemented, with men as the proposers and women as the 'upsizers'. One would assume that it would be reversed, as the women are the upsizers, but in fact, the men draw the advantage from the fact that it is their list which the flow of control moves down, thus they get their highest preference at first, while women are matched and only get to trade up if their respective preference is either already unmatched or has not gotten to his highest preference yet. Thus in stable match lists, a man ends up with the match highest on his preference list that _would_ accept him, whereas a woman ends up with the match lowest on her preference list, because she cannot guarantee that a higher preference man would propose to her, so she must accept any current partner.
 
 <br />
 
---
+++ prog-ass-2/gale_shapley_.py
@@ -0,0 +1,161 @@
+'''
+Implementing the Gale-Shapley algorithm for stable matching of pairs, in this case with men and women, each with their own respective preferences.
+
+The algorithm is implemented in the gale_shapley function, with helper functions gen_pref_list and val_stable, which generate random preference lists 
+and validate the stability of the matching, respectively. The main function generates the preference lists, runs the algorithm, and validates the result.
+
+For example, with men [A, B, C] and women [X, Y, Z], and the following preferences:
+
+A: [X, Y, Z]    X: [B, A, C]
+B: [Y, X, Z]    Y: [A, C, B]
+C: [Z, X, Y]    Z: [A, B, C]
+
+Say A proposes to X, his first choice, and X accepts. Then B proposes to Y, his first choice, and Y accepts. Finally, C proposes to Z, 
+his first choice, and Z accepts. The result would be the stable matching [(A, X), (B, Y), (C, Z)]. This is stable because no two persons prefer
+each other over their current partners. For example, A prefers X over Y, yet X prefers B over A, but B prefers Y over X. Likewise, C prefers Z over X and Y,
+yet Z prefers A and even B over C, but A prefers X over Z. Therefore, no instability is found, and the matching is stable.
+
+The Gale-Shapley algorithm is an efficient and guaranteed way to find a stable matching. It iterates through a list of unmatched men, and for each man,
+it proposes to his next most preferred woman; if she is unmatched, they are engaged, otherwise, she compares her current partner with the proposer, and
+if she prefers the proposer, she accepts him and rejects her current partner, who then becomes unmatched. This process continues until all men, and 
+consequently all women, are matched. The result is guaranteed to be stable, as no two persons prefer each other over their current partners.
+'''
+
+import random  
+import time
+import matplotlib.pyplot as plt
+
+
+def gen_rand_pref_list(n): 
+    '''
+    Generates a random preference list for n individuals.
+    
+    Parameters:
+        n (int): The number of individuals.
+        
+    Returns:
+        list: A list of preference lists, where each preference list represents the preferences of an individual.
+    '''
+    return [random.sample(range(n), n) for _ in range(n)] # Randomly sample n individuals without replacement, n times, to create n preference lists.
+
+def gale_shapley(prefs_men, prefs_women):
+    '''
+    Runs the Gale-Shapley algorithm to find a stable matching of n pairs, given the preferences of a list of men and women.
+
+    Parameters:
+        n (int): The number of pairs.
+        m_prefs (list): A list of preference lists, where each preference list represents the
+        w_prefs (list): A list of preference lists, where each preference list represents the
+
+    Returns:
+        list: A list of engagements, where each engagement is a pair of individuals.
+    '''
+    n = len(prefs_men)
+    if n != len(prefs_women):
+        raise ValueError("ERROR: Length of preference lists must be equal.")
+    
+    unmatched_men = list(range(n))
+    match_list_women = [-1] * n # -1 means unmatched
+    match_list_men = [-1] * n # -1 means unmatched  
+
+    while unmatched_men:
+        man = unmatched_men[0]
+        prefs_man = prefs_men[man]
+
+        for woman in prefs_man:
+            # if woman unmatched, match her with man
+            if match_list_women[woman] == -1:
+                match_list_women[woman] = man
+                match_list_men[man] = woman
+                unmatched_men.remove(man)
+                print(f'Matched {man} with Preference {woman}')
+                break
+            # else woman is married, check if she prefers new man, if so, match her with new man and unmatch original man
+            else:
+                curr_man = match_list_women[woman]
+                if prefs_women[woman].index(man) < prefs_women[woman].index(curr_man): # lower index --> higher preference
+                    # unmatch curr_man 
+                    unmatched_men.append(curr_man)
+                    match_list_men[curr_man] = -1
+                    # update match
+                    match_list_women[woman] = man
+                    match_list_men[man] = woman
+                    unmatched_men.remove(man)
+                    # print(f'Matched {man} with Preference {woman}')
+                    # print(f'Unmatched {curr_man}')
+                    break
+
+    # return match list
+    return [(m, w) for m, w in enumerate(match_list_men)]
+
+def val_stable(match_list, prefs_men, prefs_women):
+    '''
+    Validates the stability of a matching, given the preferences of men and women.
+
+    Parameters:
+        match_list (list): A list of engagements, where each engagement is a pair of individuals
+        prefs_men (list): A list of preference lists, where each preference list represents the preferences of that man
+        prefs_women (list): A list of preference lists, where each preference list represents the preferences of that woman
+
+    Returns:
+        bool: True if the matching is stable, False otherwise.
+        str: A message indicating whether the matching is stable or not.
+    '''
+    n = len(prefs_men)
+    if n != len(prefs_women):
+        raise ValueError("ERROR: Length of preference lists must be equal.")
+    
+    for m, w in match_list:
+        m_pref = prefs_men[m]
+
+        # check for instability against all other matches
+        for m2, w2 in match_list:
+            w_pref = prefs_women[w2]
+            if m != m2 and w != w2: # if not same match
+                # if man prefers other woman over his own AND other woman prefers man over her own
+                # print(f'Comparing {m} with {m2} and {w} with {w2}')
+                if m_pref.index(w2) < m_pref.index(w) and w_pref.index(m) < w_pref.index(m2):
+                    # print preferences
+                    print(f"Man {m}: {prefs_men[m]}"); print(f"Woman {w2}: {prefs_women[w2]}")
+                    return False, f"Instability found: Man {m} and Woman {w2} prefer each other over their current partners."
+                
+    return True, "Stable matching confirmed"
+
+def main():
+    '''
+    Generates random preference lists of sizes 10, 100, 1000 and runs the Gale-Shapley algorithm to find a stable matching for each size.
+    '''
+
+    # Generate preference lists
+    input_sizes = [10, 100, 1000, 10000, 100000]
+    exec_times = []
+    val_stable_times = []
+
+    for n in input_sizes:
+        prefs_men = gen_rand_pref_list(n)
+        prefs_women = gen_rand_pref_list(n)
+        start_time = time.time()
+        match_list = gale_shapley(prefs_men, prefs_women)
+        exec_time = time.time() - start_time
+        exec_times.append(exec_time)
+
+        # Validate stability
+        # start_time = time.time()
+        # stable, message = val_stable(match_list, prefs_men, prefs_women)
+        # val_stable_time = time.time() - start_time
+        # val_stable_times.append(val_stable_time)
+        print(f"n = {n}")
+        print(f"Matching: {match_list}")
+        # print(f"Stability: {message}")
+        print(f"Execution time: {exec_time:.6f} seconds")
+        # print(f"Validation time: {val_stable_time:.6f} seconds\n")
+
+    # Plot execution times
+    plt.plot(input_sizes, exec_times, marker='o')
+    plt.title("Execution Time vs. Input Size")
+    plt.xlabel("Input Size")
+    plt.ylabel("Execution Time (seconds)")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
